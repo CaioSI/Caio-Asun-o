f24ed780048fb9a88abc9baed59e74a7685a794f
CaioSI <63016480+CaioSI@users.noreply.github.com>
2020-04-01 16:26:21 -0300

    envio de arquivo
    Código da atividade 1

---
+++ atv1.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+public class MyClass {
+    public static void main(String args[]) {
+       
+       
+      int n1;
+      int n2;
+      
+      Scanner sc = new Scanner(System.in);
+      
+      
+      System.out.println("Digite o primeiro número: ");
+      n1 = sc.nextInt();
+      
+      System.out.println("Digite o segundo número: ");
+      n2 = sc.nextInt();
+      
+      int Prod = n1 * n2;
+      
+      System.out.println("PROD = " + Prod );
+    }
+}
